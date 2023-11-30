3036713f94e23dbc229626bb7dc20cf0d3c36913
radha <rmradha.chand@gmail.com>
2023-11-30 14:52:50 +0530

    changed struct
    changed struct

--- godep-app/main.go
+++ godep-app/main.go
@@ -1,13 +1,15 @@
-// main.go
+package godep_app// main.go
+import "fmt"
 
 package main
 
 import (
-	"fmt"
-	"github.com/Radha-Sundar/godep-app/pkg/greeter"
+"fmt"
+"github.com/Radha-Sundar/godep-app/pkg/greeter"
 )
 
 func main() {
 	message := greeter.GetGreeting()
 	fmt.Println(message)
 }
+
