3ffaa7107d7a5a9cc2102ce45a1b6fea4e8331da
radha <rmradha.chand@gmail.com>
2023-11-30 17:24:56 +0530

    removed
    removed

--- godep-app/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Radha-Sundar/godep-app
-
-go 1.21.4
--- godep-app/pkg/greeter/greeter.go
+++
@@ -1,6 +0,0 @@
-package greeter
-
-// GetGreeting returns a welcome message.
-func GetGreeting() string {
-	return "Hello from the greeter package!"
-}
